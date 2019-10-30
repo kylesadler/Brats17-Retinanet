45b24cedf08bd981fcc48945cd05ecf686d4c927
Kyle Sadler <kylesadler5@gmail.com>
2019-10-29 21:57:37 -0500

    update

--- maskrcnn_benchmark/engine/inference.py
+++ maskrcnn_benchmark/engine/inference.py
@@ -250,8 +250,8 @@ def evaluate_predictions_on_coco(
 
     coco_dt = coco_gt.loadRes(str(json_result_file))
     # coco_dt = coco_gt.loadRes(coco_results)
-    print(coco_gt)
-    print(coco_dt)
+    print(type(coco_gt))
+    print(type(coco_dt))
     exit()
     coco_eval = COCOeval(coco_gt, coco_dt, iou_type)
     coco_eval.evaluate()
