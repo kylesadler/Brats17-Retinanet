8aff9c3ebb7db60e6b717e796543df5b1cc0337f
Kyle Sadler <kylesadler5@gmail.com>
2019-11-09 18:01:19 -0600

    fixed brats_to_voc.py

--- brats/brats_to_voc.py
+++ brats/brats_to_voc.py
@@ -60,7 +60,17 @@ def main():
         for labeltype in labels:
             for l in labels[labeltype]:
                 segmentation += (raw_seg_data == l).astype(int)
-        assert (np.max(segmentation) == 3)
+
+        max_label = np.max(raw_seg_data)
+        if(max_label == 4):
+            assert (np.max(segmentation) == 3)
+        elif(max_label == 2):
+            assert (np.max(segmentation) == 2)
+        elif(max_label == 1):
+            assert (np.max(segmentation) == 1)
+        elif(max_label == 0):
+            assert (np.max(segmentation) == 0)
+
         assert (np.min(segmentation) == 0)
         segmentation = normalize(segmentation)
         
@@ -74,11 +84,8 @@ def main():
             mri_scans.append(data)
             
 
-
         for direction in directions:
             output_path = os.path.join(output_dir, direction)
-            if(os.path.exists(output_path)):
-                continue
             mkdir(output_path)
 
             slice_axis = slice_axes[direction]
@@ -91,16 +98,20 @@ def main():
                 
                 normalized_mri = []
                 for i in range(mri_scan.shape[slice_axis]):
-                    mri = np.reshape(normalize(get_slice(mri_scan, slice_axis, i)), target_shape)
-                    check_normalized(mri)
-                    normalized_mri.append(mri)
+                    mri = get_slice(mri_scan, slice_axis, i)
+
+                    if (np.max(mri) - np.min(mri) != 0):
+                        mri = normalize(mri)
+                        check_normalized(mri)
+
+                    normalized_mri.append(np.reshape(mri, target_shape))
                 
                 normalized_mri = np.concatenate(normalized_mri, axis=slice_axis)
                 assert(normalized_mri.shape == (240, 240, 155))
                 
                 mri_data.append(normalized_mri)
 
-
+            assert (len(mri_data) == 4)
             file_data = np.concatenate(mri_data, axis=slice_axis-1)
             seg_data = np.concatenate((segmentation,segmentation,segmentation,segmentation), axis=slice_axis-1)
 
@@ -328,7 +339,7 @@ def normalize(data):
 
 def save(img, slice_axis, i, folder, file_id):
     check_dims(img, slice_axis)
-    Image.fromarray(img, "L").save(os.path.join(folder, file_id + "_" + str(i) + ".png"))
+    Image.fromarray(img.astype(np.uint8), "L").save(os.path.join(folder, file_id + "_" + str(i) + ".png"))
 
 if __name__ == '__main__':
     main()
@@ -352,151 +363,6 @@ label information
     “non-enhancing (solid) core,” “necrotic (or fluid-filled) core,”
     and “non-enhancing core
 """
-"""
-def main():
-    input_dir = sys.argv[1] # where brats is located
-    output_dir = sys.argv[2] # where brats VOC is created
-    mkdir(output_dir)
-
-    directions = ["axial", "coronal", "sagittal"]
-    slice_axes = {"coronal":0, "sagittal":1, "axial":2}
-    modes = ["flair", "t1", "t2", "t1ce"]
-    labels = {"whole_tumor":[1,2,4], "tumor_core":[1,4], "enhancing_tumor":[4]}
-
-    for direction in directions:
-        output_path = os.path.join(output_dir, direction)
-        img_folder = os.path.join(output_path, "images")
-        label_folder = os.path.join(output_path, "labels")
-
-        if(os.path.exists(output_path)):
-            continue
-
-        mkdir(output_path)
-        mkdir(img_folder)
-        mkdir(label_folder)
-
-        file_ids = [get_file_id(x) for x in os.listdir(os.path.join(input_dir, "seg"))]
-        slice_axis = slice_axes[direction]
-
-        # print('file_ids')
-        # print(file_ids)
-
-        for file_id in file_ids: # process each set of files
-
-            # load segmentation data
-            raw_seg_data = nibabel.load(os.path.join(input_dir, "seg", file_id + "_seg.nii")).get_data()
-            assert (raw_seg_data.shape == (240, 240, 155))
-
-            # convert to whole_tumor:1, enhancing_tumor:2, and tumor_core:3, else:0 labels
-            segmentation = np.zeros(raw_seg_data.shape).astype(int)
-            for labeltype in labels:
-                for l in labels[labeltype]:
-                    segmentation += (raw_seg_data == l).astype(int)
-
-            
-            assert (np.max(segmentation) == 3)
-            assert (np.min(segmentation) == 0)
-            segmentation = normalize(segmentation)
-            seg_data = np.concatenate((segmentation,segmentation,segmentation,segmentation), axis=slice_axis - 1)
-
-
-            # load all mri scans from patient
-            file_data = [] # "flair", "t1", "t2", "t1ce"
-            for mode in modes:
-                data = nibabel.load(os.path.join(input_dir, mode, file_id+"_"+mode+".nii")).get_data()
-                assert (data.shape == (240, 240, 155))
-                
-                normalized_mri = []
-                for i in range(data.shape[slice_axis]):
-                    target_shape = [240, 240, 155]
-                    target_shape[slice_axis] = 1
-                    normalized_mri.append(np.reshape(normalize(get_slice(data, slice_axis, i)), target_shape))
-                normalized_mri = np.concatenate(normalized_mri, axis=slice_axis)
-                assert(normalized_mri.shape == (240, 240, 155))
-                file_data.append(normalized_mri)
-
-            file_data = np.concatenate(file_data, axis=slice_axis-1)
-
-
-            assert(file_data.shape == seg_data.shape)
-            assert (np.max(seg_data) == 255)
-            assert (np.min(seg_data) == 0)
-            assert (np.max(file_data) == 255)
-            assert (np.min(file_data) == 0)
-
-
-            # slice images, normalize, and save
-            for i in range(file_data.shape[slice_axis]):
-
-                img = get_slice(file_data, slice_axis, i)
-                seg = get_slice(seg_data, slice_axis, i)
-
-                if(np.max(img) - np.min(img) == 0 and np.max(seg) - np.min(seg) == 0):
-                    continue
-
-                save(img, slice_axis, i, img_folder, file_id)
-                save(seg, slice_axis, i, label_folder, file_id)
-                
-                # slice_and_save(file_data, slice_axis, i, img_folder, file_id)
-                # slice_and_save(seg_data, slice_axis, i, label_folder, file_id)
-"""               
-"""
-def slice_and_save(data, slice_axis, i, folder, file_id):
-
-    img = get_slice(data, slice_axis, i)
-
-    if(np.max(img) - np.min(img) == 0):
-        return
-
-    check_dims(img, slice_axis)
-
-    Image.fromarray(img, "L").save(os.path.join(folder, file_id + "_" + str(i) + ".png"))
-"""
-'''
-def create_data(input_path, seg_path,  output_img, output_label, output_mask, subsec0, seg_file_end, flair_label):
-    input_list = dir([input_path, '*.nii']);
-    for i = 1 : length(input_list)   % for each file in input_dir   
-        input_name = input_list(i).name;
-        seg_name = strrep(input_name, subsec0, seg_file_end);
-        if isfile([input_path, input_name]) && isfile([seg_path, seg_name])
-            info  = nii_read_header([input_path, input_name]);
-            V = nii_read_volume(info); % mode
-            info  = nii_read_header([seg_path, seg_name]);
-            V1 = nii_read_volume(info); % seg
-            for j = 1 : size(V, 3)
-                I = V(:,:,j);
-                I = double(I);
-                dif = max(I(:)) - min(I(:));
-                if dif == 0 
-                    continue;
-                else
-                	I = (I - min(I(:)))/dif;
-                    I1 = V1(:,:,j);
-                    I1 = double(I1);
-                    label = zeros(size(I1)); % non brain part
-                    label(I>0)=1; % brain
-                    for k = 1 : size(flair_label,2)
-                        label(I1==flair_label(k)) = 2; % tumor
-                    end
-
-                    label = imfill(label, 'holes');
-                    label = double(label);
-                end
-                I = im2uint8(I);
-                final_im =cat(3, I, I, I);
-                imwrite(final_im, [output_img, strrep(input_name, subsec0, ['_', num2str(j), '.png'])]);
-                final_label = uint8(label);
-                imwrite(final_label, [output_label, strrep(input_name, subsec0, ['_', num2str(j), '.png'])]);
-
-
-            end
-        end
-
-
-
-
-    end
-'''
 """
 def collapse_this():
     % flair_label = [1, 2, 3, 4]; %whole tumor
