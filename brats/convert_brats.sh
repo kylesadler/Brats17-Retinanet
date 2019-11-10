87ef91f8a4819651a1a3ee7841d4c44eaea83212
Kyle Sadler <kylesadler5@gmail.com>
2019-11-09 18:18:11 -0600

    fixed brats_to_voc.py

--- brats/convert_brats.sh
+++ brats/convert_brats.sh
@@ -2,46 +2,46 @@
 
 # git pull; nohup ./convert_brats.sh /home/kyle/datasets/MICCAI_BraTS17_Data_Training_for_NLe.zip /home/kyle/datasets/test_bratscoco/ &
 
-#if [ -z $1 ]
-#then
-#echo "usage:"
-#echo "convert_brats.sh /path/to/brats.zip /path/to/created/brats_coco/"
-#exit 1
-#
-#fi
-#
-#move_files(){
-#	files=("$1"/*)
-#	size=${#files[@]}
-#
-#	for ((i=0;i<$size;i++));
-#	do
-#	mv ${files[$i]}/*flair.nii.gz flair
-#	mv ${files[$i]}/*t2.nii.gz t2
-#	mv ${files[$i]}/*t1ce.nii.gz t1ce
-#	mv ${files[$i]}/*t1.nii.gz t1
-#	mv ${files[$i]}/*seg.nii.gz seg
-#	done
-#}
-#
+if [ -z $1 ]
+then
+echo "usage:"
+echo "convert_brats.sh /path/to/brats.zip /path/to/created/brats_coco/"
+exit 1
+
+fi
+
+move_files(){
+	files=("$1"/*)
+	size=${#files[@]}
+
+	for ((i=0;i<$size;i++));
+	do
+	mv ${files[$i]}/*flair.nii.gz flair
+	mv ${files[$i]}/*t2.nii.gz t2
+	mv ${files[$i]}/*t1ce.nii.gz t1ce
+	mv ${files[$i]}/*t1.nii.gz t1
+	mv ${files[$i]}/*seg.nii.gz seg
+	done
+}
+
 start=$(pwd)
 
 zip_file=$1
 cd $(dirname "${zip_file}")
-#unzip $(basename "${zip_file}") # MICCAI_BraTS17_Data_Training_for_NLe.zip
+unzip $(basename "${zip_file}") # MICCAI_BraTS17_Data_Training_for_NLe.zip
 cd MICCAI_BraTS17_Data_Training/
-#rm survival_data.csv
-#mkdir flair/ t1/ t1ce/ t2/ seg/
-#
-#move_files "HGG"
-#move_files "LGG"
-#
-#rm -r HGG LGG
-#gunzip -vr *
-#
+rm survival_data.csv
+mkdir flair/ t1/ t1ce/ t2/ seg/
+
+move_files "HGG"
+move_files "LGG"
+
+rm -r HGG LGG
+gunzip -vr *
+
 if [ -z $2 ]
 then
-brats_voc_dir=$(dirname "${zip_file}")'/brats2017_voc/'
+brats_voc_dir=$(dirname "${zip_file}")'/brats2017_coco/'
 else
 brats_voc_dir=$2
 fi
